ae05c64450fe3560b471c17f297c927a2857b3ae
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-08-22 23:54:44 +0200

    fix: change welcome to button

--- app/index.js
+++ app/index.js
@@ -1,17 +1,19 @@
-import { Link } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { Link, router } from "expo-router";
+import { View, Button, StyleSheet } from "react-native";
 
 function HomeScreen() {
     return (
-        <View style={styles.container}> 
-            <Link href='/menu'>Go to Menu</Link>
+        <View style={styles.container}>
+                <Button title="Welcome" onPress={() => {
+                    router.push('/menu');
+                }} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     }
